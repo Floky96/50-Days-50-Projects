3bf415ece0492281a4e244e16c8a4b56f32c96cc
Floky96 <111383186+Floky96@users.noreply.github.com>
2022-12-11 23:04:19 +0100

    Day 24 | Created Content Placeholder

---
+++ Day 24 - Content Placeholder/js/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="./img/sunsetvalley.jpg" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML =
+    '<img src="./img/Fox-cropped.jpg" alt="" />'
+  name.innerHTML = 'Floky M.'
+  date.innerHTML = 'Dec 10, 2022'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
