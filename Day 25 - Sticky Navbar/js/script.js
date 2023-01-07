2da85d1407a071bb3acf757253eba76c2b686f06
Floky96 <111383186+Floky96@users.noreply.github.com>
2023-01-07 02:04:56 +0100

    Repaired Laptop, Back to coding :)
    Had my laptop repaired after a SSD nuke. And back to grinding it is ^^

--- Day 25 - Sticky Navbar/js/script.js
+++ Day 25 - Sticky Navbar/js/script.js
@@ -4,9 +4,10 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if (window.scrollY > nav.offsetHeight + 150) {
+    if(window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active')
-    } else {
+    }
+    else {
         nav.classList.remove('active')
     }
 }
